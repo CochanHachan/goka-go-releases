52c252932ce8aee4bb6bcc9c128b061295c2dd38
松浦康樹 <kokimatsuura@outlook.com>
2026-05-04 20:53:16 +0000

    Merge pull request 37 from bump-v1.2.183 into main

--- igo/constants.py
+++ igo/constants.py
@@ -28,7 +28,7 @@ if _c is None:
     raise RuntimeError("Unknown _ENV={!r}".format(_ENV))
 
 APP_NAME          = _c["app_name"]
-APP_VERSION       = "1.2.182"
+APP_VERSION       = "1.2.183"
 APP_BUILD         = "20260503"
 STAGING_LABEL     = _c["staging_label"]
 CLOUD_SERVER_URL  = _c["cloud_server_url"]
